a58c846fe312991ca3ca97cd6b958d749faf88fb
John Bernal <john.bernal@mail.utoronto.ca>
2018-02-14 11:06:41 -0500

    Player class basic implementation

---
+++ Clash of PlayerKnown's Geometry Wars Doki Doki Club 'A StarWars Story'/src/Player.java
@@ -0,0 +1,36 @@
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.Observable;
+import java.util.Observer;
+
+/* The player class object */
+public class Player extends MovableObject implements Observer{
+	
+	public void collision() {
+		
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean intersects(Rectangle r) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
