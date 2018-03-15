74ff42929fbbf2ae8ef7a890c286b57643f84be5
bferna <brandon.fernandez@mail.utoronto.ca>
2018-03-15 17:58:13 -0400

    # WARNING: head commit changed in the meantime
    player wall collision
    
    Created a marker for the player, and the angle variable points to the
    direction the player faces/moves, 0 angle points to the right.

--- Clash of PlayerKnown's Geometry Wars Doki Doki Club 'A StarWars Story'/src/gameResources/Player.java
+++ Clash of PlayerKnown's Geometry Wars Doki Doki Club 'A StarWars Story'/src/gameResources/Player.java
@@ -7,11 +7,13 @@ import java.awt.event.KeyEvent;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+import java.math.*;
 
 /* The player class object */
 public class Player extends MovableObject implements Observer{
 	
 	final static int WALK = 5;
+	final static int ROTSPEED = 1;
 	public final static int WIDTH = 20;
 	public final static int HEIGHT = 20;
 	private GameObjectHandlerView gohv;
@@ -36,7 +38,10 @@ public class Player extends MovableObject implements Observer{
 		this.color = color;
 		this.name = name;
 		this.gohv = gohv;
-		this.angle = 0;
+		
+		//this.leftR = leftR;
+		//this.rightR = rightR;
+		//this.angle = angle;
 	}
 	
 	public Player(String name, GameObjectHandlerView gohv){
@@ -51,6 +56,9 @@ public class Player extends MovableObject implements Observer{
 		this.down = KeyEvent.VK_S;
 		this.right = KeyEvent.VK_D;
 		this.left = KeyEvent.VK_A;
+		
+		//this.leftR = KeyEvent.VK_Q;
+		//this.rightR = KeyEvent.VK_E;
 		this.angle = 0;
 	}
 	
@@ -137,6 +145,8 @@ public class Player extends MovableObject implements Observer{
 		//shall input an equal negative velocity
 		// could open a bug where one key is not recorded 
 		// it will have twice the speed
+		
+		//horizontal movement
 		if(key == this.up) {
 			if(keyAction == KeyEvent.KEY_PRESSED) {
 				//this.setVelY(-WALK);
@@ -144,8 +154,7 @@ public class Player extends MovableObject implements Observer{
 			}else if (keyAction == KeyEvent.KEY_RELEASED) {
 				//this.setVelY(0);
 				this.moveUp = false;
-			}
-			
+			}	
 		}else if(key == this.down) {
 			if(keyAction == KeyEvent.KEY_PRESSED) {
 				//this.setVelY(WALK);
@@ -156,6 +165,7 @@ public class Player extends MovableObject implements Observer{
 			}
 		}
 		
+		//vertical movement
 		if(key == this.left) {
 			if(keyAction == KeyEvent.KEY_PRESSED) {
 				//this.setVelX(-WALK);
@@ -163,8 +173,7 @@ public class Player extends MovableObject implements Observer{
 			}else if (keyAction == KeyEvent.KEY_RELEASED) {
 				//this.setVelX(0);
 				this.moveLeft = false;
-			}
-			
+			}	
 		}else if(key == this.right) {
 			if(keyAction == KeyEvent.KEY_PRESSED) {
 				//this.setVelX(WALK);
@@ -173,19 +182,51 @@ public class Player extends MovableObject implements Observer{
 				//this.setVelX(0);
 				this.moveRight = false;
 			}
-		}else if(key == KeyEvent.VK_Q) {
+		}
+		
+		//Rotation
+		if(key == KeyEvent.VK_Q) {
 			if(keyAction == KeyEvent.KEY_PRESSED) {
 				//this.setVelX(WALK);
-				this.angle = (this.angle-45)%360;
+				this.angle = (this.angle-ROTSPEED)%360;
 			}
 		}else if(key == KeyEvent.VK_E) {
 			if(keyAction == KeyEvent.KEY_PRESSED) {
 				//this.setVelX(WALK);
-				this.angle = (this.angle+45)%360;
+				this.angle = (this.angle+ROTSPEED)%360;
 			}
 		}
 	}
 
+	private void setRotation() {
+		int vertical = 0;
+		int horizontal = 0;
+		int tempAngle = 0;
+		if (this.moveDown) vertical--;
+		if (this.moveUp)	 vertical ++;
+		if (this.moveRight) horizontal ++;
+		if (this.moveLeft) horizontal --;
+		
+		if(vertical == 0 || horizontal == 0) {// on key is pressed
+			if (this.moveDown) this.angle =270;
+			else if (this.moveUp) this.angle =90;
+			else if (this.moveRight) this.angle =0;
+			else if (this.moveLeft) this.angle =180;
+		}else {
+			tempAngle = (int)Math.toDegrees(Math.atan(vertical/horizontal));
+			if(tempAngle< 0) tempAngle += 180; //can't do modular
+			if(vertical > 0 ) {
+				this.angle = tempAngle;
+			}else {
+				this.angle = 180 + tempAngle;
+			}
+		}
+		//System.out.println(this.name +": "+ tempAngle +" a:"+ this.angle);
+		
+	}
+	
+	
+	
 	@Override
 	public void tick() {
 		
@@ -199,9 +240,11 @@ public class Player extends MovableObject implements Observer{
 		if (this.moveRight) this.velX+=this.WALK;
 		if (this.moveLeft) this.velX-=this.WALK;
 		
-		// TODO Auto-generated method stub
-		collision();
+
+		//remember that the y coordinates are inverted
+		setRotation();
 		
+		collision();
 		
 		//REPLACE: KEEPING PLAY WITHIN GAME WALLS
 		if (this.x >= (Game.WIDTH-this.width-5))
@@ -214,11 +257,28 @@ public class Player extends MovableObject implements Observer{
 			this.y = 0;
 	}
 
+	private void drawMarker(Graphics g) {
+		int centerX = this.x+10;
+		int centerY = this.y+10;
+		double radAngle;
+		int angleDispX ;
+		int angleDispY ;
+		
+		g.setColor(Color.GREEN);
+		for(int i=-2; i<=2; i++) {
+			radAngle = Math.toRadians(this.angle + (10*i));
+			angleDispX = (int)(9*Math.cos(radAngle));
+			angleDispY = (int)(9*Math.sin(radAngle));
+			g.drawLine(centerX, centerY, centerX+angleDispX, centerY-angleDispY);
+		}
+	}
+	
 	@Override
 	public void render(Graphics g) {
 		// TODO Auto-generated method stub
 		g.setColor(this.color);
 		g.fillRect(this.x, this.y, this.width, this.height);
+		drawMarker(g);
 	}
 
 	@Override
